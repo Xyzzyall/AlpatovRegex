da06f690db524c26d87c5a4f978edb672e7bee00
xyzzy <xyzzyall@outlook.com>
2021-06-06 14:07:48 +0300

    первая тестовая версия и ещё работает

--- src/NumbersFromRussian.java
+++ src/NumbersFromRussian.java
@@ -1,38 +1,39 @@
+import java.util.Locale;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class NumbersFromRussian
 {
     protected static RegexRule[] rules = {
-            new RegexRule("[a]", "0", -1),
-            new RegexRule("[b]", "1", -1),
-            new RegexRule("[c]", "2", -1),
-            new RegexRule("[d]", "3", -1),
-            /*new RegexRule("", "4"),
-            new RegexRule("", "5"),
-            new RegexRule("", "6"),
-            new RegexRule("", "7"),
-            new RegexRule("", "8"),
-            new RegexRule("", "9"),
-            new RegexRule("", "10"),
-            new RegexRule("", "11"),
-            new RegexRule("", "12"),
-            new RegexRule("", "13"),
-            new RegexRule("", "14"),
-            new RegexRule("", "15"),
-            new RegexRule("", "16"),
-            new RegexRule("", "17"),
-            new RegexRule("", "18"),
-            new RegexRule("", "19"),
-            new RegexRule("", "20"),
-            new RegexRule("", "30"),
-            new RegexRule("", "40"),
-            new RegexRule("", "50"),
-            new RegexRule("", "60"),
-            new RegexRule("", "70"),
-            new RegexRule("", "80"),
-            new RegexRule("", "90"),
-            new RegexRule("", "100"),*/
+            new RegexRule("(^ноль$)|((^нол)[юеях]$)", "0", -1),
+            new RegexRule("(^один$)|((^одн)[огим]$)", "1", -1),
+            new RegexRule("(^два$)|(^дву)[а-я]$", "2", -1),
+            new RegexRule("(^три$)|(^тр)[ёех]$", "3", -1),
+            new RegexRule("(^четыре$)|(^четыр)[ёх]$", "4", -1),
+            new RegexRule("(^пять$)|(^пят)[и]$", "5", -1),
+            new RegexRule("(^шесть$)|(^шест)[и]$", "6", -1),
+            new RegexRule("(^семь$)|(^сем)[и]$", "7", -1),
+            new RegexRule("(^восемь$)|(^восьм)[и]$", "8", -1),
+            new RegexRule("(^девять$)|(^девят)[и]$", "9", -1),
+            new RegexRule("(^десять$)|(^десят)[и]$", "10", -1),
+            new RegexRule("(^одиннадцать$)|(^одиннадцат)[и]$", "11", -1),
+            new RegexRule("(^двенадцать$)|(^двенадцат)[и]$", "12", -1),
+            new RegexRule("(^тринадцать$)|(^тринадцат)[и]$", "13", -1),
+            new RegexRule("(^четырнадцать$)|(^четырнадцат)[и]$", "14", -1),
+            new RegexRule("(^пятнадцать$)|(^пятнадцат)[и]$", "15", -1),
+            new RegexRule("(^шестнадцать$)|(^шестнадцат)[и]$", "16", -1),
+            new RegexRule("(^семнадцать$)|(^семнадцат)[и]$", "17", -1),
+            new RegexRule("(^восемнадцать$)|(^восемнадцати)[и]$", "18", -1),
+            new RegexRule("(^девятнадцать$)|(^девятнадцат)[и]$", "19", -1),
+            new RegexRule("(^двадцать$)|(^двадцат)[и]$", "20", 9),
+            new RegexRule("(^тридцать$)|(^тридцат)[и]$", "30", 9),
+            new RegexRule("(^сорок$)|(^сорок)[а]$", "40", 9),
+            new RegexRule("(^пятдесят$)|(^пятидесят)[и]$", "50", 9),
+            new RegexRule("(^шестьдесят$)|(^шестидесят)[и]$", "60", 9),
+            new RegexRule("(^семьдесят$)|(^семидесят)[и]$", "70", 9),
+            new RegexRule("(^восемьдесят$)|(^восьмидесят)[и]$", "80", 9),
+            new RegexRule("(^девяносто$)|(^девяност)[а]$", "90", 9),
+            new RegexRule("(^сто$)|(^ста$)", "100", 27),
     };
 
     protected String text;
@@ -116,7 +117,7 @@ public class NumbersFromRussian
 
         public String process(String text) throws NoMatch
         {
-            Matcher matcher = pattern.matcher(text);
+            Matcher matcher = pattern.matcher(text.toLowerCase(Locale.ROOT));
             if (!matcher.matches()) throw new NoMatch();
             return matcher.replaceAll(replace_to);
         }
